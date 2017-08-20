72486ac2313a50231f3a360c79b04f9633d6a332
panlopburi <panlopburi@hotmail.com>
2017-08-20 15:45:38 +0700

    'name'

--- data.py
+++ data.py
@@ -1,14 +1,14 @@
 data = {
-    'name': 'James Tiberius Kirk',
-    'nickname': 'Jim',
-    'image': 'kirk.jpg',
+    'name': 'Pan LopBuri',
+    'nickname': 'Pan',
+    'image': 'pan.jpg',
     'greeting_msg': 'Hi, there!',
     'about': """
-        James Tiberius "Jim" Kirk was a male Human Starfleet officer in the 23rd century. He was arguably one of the most famous and highly decorated starship captains in the history of Starfleet. As the commanding officer of the Constitution-class starships USS Enterprise and USS Enterprise-A, Kirk served United Federation of Planets interests as an explorer, soldier, time-traveler, and diplomat.
+        Pan Lopburi \n 
     """,
     'work': {
-        'position': 'Starfleet officer',
-        'at': 'Starfleet'
+        'position': 'KMITL officer',
+        'at': 'KMITL'
     },
     'courses': [
         {
@@ -28,16 +28,16 @@ data = {
         },
         {
             'name': 'Klingon Physiology',
-            'school': 'Valcan State Institute of Technology',
+            'school': 'KMITL',
             'url': 'klingon-phys'
         }
     ],
     'skills': [
-        'Leadership',
-        'Leadership',
-        'Human Language',
-        'Borg Language',
-        'Valcan Language',
+        'CSGO',
+        'PANGYA',
+        'PB',
+        'PUBG',
+        'OVERWATCH',
         'Sleeping'
     ]
 }
